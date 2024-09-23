79283e38278f0e09a0438eadce5f0387aafe3363
nemo <nemo724@naver.com>
2024-09-23 21:29:40 +0900

    x와y 좌표 입력 함수 추가

--- 백준2563/색종이 덮기.c
+++ 백준2563/색종이 덮기.c
@@ -5,14 +5,15 @@ int y_coordinate = 0;
 int pappers_count = 0;
 
 void input_pappers_count(void);
-
+void input_xy_coordinates(void);
 
 
 
 void input_pappers_count(void)
 {scanf_s("%d", &pappers_count);}
 
-
+void input_xy_coordinates(void)
+{scanf_s("%d%d", &x_coordinate, &y_coordinate);}
 
 
 
