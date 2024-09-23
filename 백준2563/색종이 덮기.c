72cfa067481560a964d109265a0029ab573da980
nemo <nemo724@naver.com>
2024-09-23 21:35:25 +0900

    입력 받은 좌표를 기준으로 공간을 채우는 함수 추가

--- 백준2563/색종이 덮기.c
+++ 백준2563/색종이 덮기.c
@@ -6,6 +6,7 @@ int pappers_count = 0;
 
 void input_pappers_count(void);
 void input_xy_coordinates(void);
+void fill_area(void);
 
 
 
@@ -15,7 +16,14 @@ void input_pappers_count(void)
 void input_xy_coordinates(void)
 {scanf_s("%d%d", &x_coordinate, &y_coordinate);}
 
-
+void fill_area(void)
+{int max_x_coordinate = x_coordinate + 10;
+int max_y_coordinate = y_coordinate + 10;
+for (int x = x_coordinate; x <= max_x_coordinate; x++)
+{for (int y = y_coordinate; y <= max_y_coordinate; y++)
+{papper[x][y] = 1;}
+}
+}
 
 
 
