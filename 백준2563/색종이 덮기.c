c7deb7ce336c626fa68034a053f5cbbe87005a28
nemo <nemo724@naver.com>
2024-09-23 21:28:04 +0900

    종이 갯수 입력 함수 추가

--- 백준2563/색종이 덮기.c
+++ 백준2563/색종이 덮기.c
@@ -3,3 +3,38 @@ int papper[100][100];
 int x_coordinate = 0;
 int y_coordinate = 0;
 int pappers_count = 0;
+
+void input_pappers_count(void);
+
+
+
+
+void input_pappers_count(void)
+{scanf_s("%d", &pappers_count);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(void)
+{
+
+
+
+
+
+
+
+
+
+}
